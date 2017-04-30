189be48ba4c5b74cde26ac32a1cea30746d4f09a
lachlan <lachlan.miller.1990@outlook.com>
2017-04-30 12:25:15 +0900

    Add a procfile for to let react frontend query the rails api

--- test/controllers/japanese_words_controller_test.rb
+++ test/controllers/japanese_words_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class JapaneseWordsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    JapaneseWord.create(word: "漢字", meaning: "Kanji characters")
+    JapaneseWord.create(word: "漢字検定", meaning: "Kanji test")
+    JapaneseWord.create(word: "文字通り", meaning: "Literally")
+  end
+
+  test "returns some words based on kanji" do
+    get japanese_words_path, params: {
+      characters: [ "漢","字" ]
+    }
+    json = JSON.parse(response.body)
+
+    assert_equal json.length, 2
+  end
 end
